47c17ea0e70ca45bc34c127b7c90f241a2e8509e
Lautaro <Lauchx@users.noreply.github.com>
2024-07-08 21:33:35 -0300

    api premier league create table

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 let apiData = [];
 function getApiJson() {
     return new Promise((resolve, reject) => {
-        fetch("https://pokeapi.co/api/v2/pokemon/ditto").then(api => {
+        fetch("https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json").then(api => {
             if (!api.ok) {
               throw new Error('Network response was not ok');
             }
-            return response.json(); 
-          }).then(api => {
-            resolve(api.results)
+            return api.json(); 
+          }).then(apis => {
+            resolve(apis.clubs)
         }).catch(error => {
-            //reject(error);
+            
             throw Error("entro aca");
         })
     })
 }
 
 function returnApiData() {
-    getApiJson().then(apiData => {
-        console.log(apiData)
-        resolve(apiData)
+    getApiJson().then(apisData => {
+     let tbody = document.getElementById("table").getElementsByTagName("tbody")[0]
+     tbody.innerHTML = "";
+
+    
+     apisData.forEach(apiData => {
+        let newRow = tbody.insertRow();
+        let cell1 = newRow.insertCell();
+        let cell2 = newRow.insertCell();
+
+        cell1.innerHTML = apiData.code;
+        cell2.innerHTML = apiData.name;
+    });
+
+       // resolve(apisData)
     })
     .catch(error => {
-        reject(error);
-        throw Error("noooo no se que paso kumpa");
+        throw Error("noooo no se que paso!!!!11!! ", error);
     })
-    console.log(apiData)
+    
 }
