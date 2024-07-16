10f17de9b0065ae1bd1d8373c51d894cc49827e0
Lautaro <Lauchx@users.noreply.github.com>
2024-07-16 20:17:23 -0300

    api champions, 2table

--- script.js
+++ script.js
@@ -1,39 +1,64 @@
 let apiData = [];
 function getApiJson() {
     return new Promise((resolve, reject) => {
-        fetch("https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json").then(api => {
+        fetch("https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/cl.groups.json").then(api => {
             if (!api.ok) {
               throw new Error('Network response was not ok');
             }
             return api.json(); 
           }).then(apis => {
-            resolve(apis.clubs)
+            resolve(apis.groups)
         }).catch(error => {
             
-            throw Error("entro aca");
+            throw Error("Error en fetch: ", error);
         })
     })
 }
 
 function returnApiData() {
     getApiJson().then(apisData => {
-     let tbody = document.getElementById("table").getElementsByTagName("tbody")[0]
+     let tbody = document.getElementById("Groups").getElementsByTagName("tbody")[0]
      tbody.innerHTML = "";
 
     
      apisData.forEach(apiData => {
         let newRow = tbody.insertRow();
-        let cell1 = newRow.insertCell();
-        let cell2 = newRow.insertCell();
+        let cellGroup = newRow.insertCell();
+        let cellTeams = newRow.insertCell();
 
-        cell1.innerHTML = apiData.code;
-        cell2.innerHTML = apiData.name;
-    });
-
-       // resolve(apisData)
-    })
-    .catch(error => {
-        throw Error("noooo no se que paso!!!!11!! ", error);
+        cellGroup.innerHTML = apiData.name;
+        cellTeams.innerHTML = apiData.teams[0]+ "<br>";
+        cellTeams.innerHTML +=  apiData.teams[1] + "<br>" ;
+        cellTeams.innerHTML +=  apiData.teams[2]+ "<br>";
+        cellTeams.innerHTML +=  apiData.teams[3]+ "<br>";
+    });}).catch(error => {
+        throw Error("Error en Promise: ", error);
     })
     
+}
+function addObject() {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest()
+        xhr.open('POST', "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/cl.groups.json")
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        var data = JSON.stringify({
+            'Team1': document.getElementById('team1').value,
+            'Team2': document.getElementById('team2').value
+        })
+        var object = JSON.stringify({
+            'name': document.getElementById('name').value,
+            'data': data
+        })
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                resolve(xhr.response)
+            } else {
+                reject(Error(xhr.statusText))
+            }
+        }
+        xhr.onerror = function () {
+            reject(Error('Error: unexpected network error.'))
+        }
+        xhr.send(object)
+    })
 }
